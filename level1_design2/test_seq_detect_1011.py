96f8bd3695293fbb2e4ceb106ec89ea13cba9634
ThomasMJosline <thomasm@iitbhilai.ac.in>
2022-07-24 08:30:43 +0000

    first try

--- level1_design2/test_seq_detect_1011.py
+++ level1_design2/test_seq_detect_1011.py
@@ -23,4 +23,61 @@ async def test_seq_bug1(dut):
     dut.reset.value = 0
     await FallingEdge(dut.clk)
 
-    cocotb.log.info('#### CTB: Develop your test here! ######')
+    cocotb.log.info('#### TEST  FOR  SEQUENCE DETECTOR #####')
+
+    dut.inp_bit.value=1
+
+    dut._log.info(f'SEQ_SEEN={int(dut.seq_seen.value):03}')
+
+    await FallingEdge(dut.clk)
+
+    dut.inp_bit.value=0
+
+    dut._log.info(f'SEQ_SEEN={int(dut.seq_seen.value):03}')
+
+    await FallingEdge(dut.clk)
+
+    dut.inp_bit.value=1
+
+    dut._log.info(f'SEQ_SEEN={int(dut.seq_seen.value):03}')
+
+    await FallingEdge(dut.clk)
+
+    dut.inp_bit.value=1
+
+    dut._log.info(f'SEQ_SEEN={int(dut.seq_seen.value):03}')
+
+    await FallingEdge(dut.clk)
+
+    dut._log.info(f'SEQ_SEEN={int(dut.seq_seen.value):03}')
+
+    assert dut.seq_seen.value == 1, "Test failed, because seq_seen output should be {B} but the output from DUT is {OUT} ".format(B=1, OUT=dut.seq_seen.value)
+
+@cocotb.test()
+async def test_seq_bug2(dut):
+    """Test for seq detection """
+
+    clock = Clock(dut.clk, 10, units="us")  # Create a 10us period clock on port clk
+    cocotb.start_soon(clock.start())        # Start the clock
+
+    # reset
+    dut.reset.value = 1
+    await FallingEdge(dut.clk)  
+    dut.reset.value = 0
+    await FallingEdge(dut.clk)
+
+    cocotb.log.info('#### TEST  FOR  SEQUENCE DETECTOR #####')
+
+    A=[1,0,1,1]
+
+    for i in A:
+        dut.inp_bit.value=i
+
+        dut._log.info(f'SEQ_SEEN={int(dut.seq_seen.value):03}')
+        
+        await FallingEdge(dut.clk)
+    
+    await FallingEdge(dut.clk)
+    dut._log.info(f'SEQ_SEEN={int(dut.seq_seen.value):03}')
+
+    assert dut.seq_seen.value == 1, "Test failed, because seq_seen output should be {B} but the output from DUT is {OUT} ".format(B=1, OUT=dut.seq_seen.value)
