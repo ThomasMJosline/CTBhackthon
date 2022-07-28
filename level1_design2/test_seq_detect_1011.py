747bd4d01da2c47e00b1a87fd600b0ce1b5d864d
ThomasMJosline <thomasm@iitbhilai.ac.in>
2022-07-28 09:09:16 +0000

    updated with 2 test

--- level1_design2/test_seq_detect_1011.py
+++ level1_design2/test_seq_detect_1011.py
@@ -56,6 +56,9 @@ async def test_seq_bug1(dut):
 
     assert dut.seq_seen.value == 1, "Test failed, because seq_seen output should be {B} but the output from DUT is {OUT} ".format(B=1, OUT=dut.seq_seen.value)
 
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value=0
+
 '''@cocotb.test()
 async def test_seq_bug2(dut):
     """Test for seq detection """
@@ -87,3 +90,54 @@ async def test_seq_bug2(dut):
     dut._log.info(f'SEQ_SEEN={int(dut.seq_seen.value):03}')
 
     assert dut.seq_seen.value == 1, "Test failed, because seq_seen output should be {B} but the output from DUT is {OUT} ".format(B=1, OUT=dut.seq_seen.value)'''
+
+@cocotb.test()
+async def test_seq_bug2(dut):
+    """Test for seq detection """
+
+    clock = Clock(dut.clk, 10, units="us")  # Create a 10us period clock on port clk
+    cocotb.start_soon(clock.start())        # Start the clock
+
+    # reset
+    dut.reset.value = 1
+    await FallingEdge(dut.clk)  
+    dut.reset.value = 0
+    await FallingEdge(dut.clk)
+
+    cocotb.log.info('#### TEST  FOR  SEQUENCE DETECTOR #####')
+
+    dut.inp_bit.value=1
+
+    dut._log.info(f'INP_BIT={1:03}  SEQ_SEEN={int(dut.seq_seen.value):03}')
+
+    await FallingEdge(dut.clk)
+
+    dut.inp_bit.value=0
+
+    dut._log.info(f'INP_BIT={0:03} SEQ_SEEN={int(dut.seq_seen.value):03}')
+
+    await FallingEdge(dut.clk)
+
+    dut.inp_bit.value=1
+
+    dut._log.info(f'INP_BIT={1:03} SEQ_SEEN={int(dut.seq_seen.value):03}')
+
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value=0
+
+    dut._log.info(f'INP_BIT={0:03} SEQ_SEEN={int(dut.seq_seen.value):03}')
+
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value=1
+    dut._log.info(f'INP_BIT={1:03} SEQ_SEEN={int(dut.seq_seen.value):03}')
+
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value=1
+    dut._log.info(f'INP_BIT={1:03} SEQ_SEEN={int(dut.seq_seen.value):03}')
+
+
+    await FallingEdge(dut.clk)
+
+    dut._log.info(f'SEQ_SEEN={int(dut.seq_seen.value):03}')
+
+    assert dut.seq_seen.value == 1, "Test failed, because seq_seen output should be {B} but the output from DUT is {OUT} ".format(B=1, OUT=dut.seq_seen.value)
